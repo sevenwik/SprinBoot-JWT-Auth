b43cd683fe5eda6a48e168acb5018c681170ffc6
Karambeium <kjivani07@gmail.com>
2024-08-07 15:59:12 +0000

    port 8081, added token and register paths

--- jwt-auth/src/main/java/com/api/jwt_auth/controller/AuthController.java
+++ jwt-auth/src/main/java/com/api/jwt_auth/controller/AuthController.java
@@ -1,3 +1,18 @@
+package com.api.jwt_auth.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+@CrossOrigin
 public class AuthController {
-    
+    @GetMapping("/token")
+    public void getToken(@RequestBody String username, @RequestBody String password) {
+        
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody String username, @RequestBody String password) {
+
+    }
 }
--- jwt-auth/src/main/java/com/api/jwt_auth/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.jwt_auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.jwt_auth.model.*;
-
-public interface AuthRepository extends JpaRepository<AuthModel, Integer>{
-
-}
